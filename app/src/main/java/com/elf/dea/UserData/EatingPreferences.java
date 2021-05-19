6b7a3173ef310289f392a84eeb5e4cda858898bd
elifkoseler <koseler17@itu.edu.tr>
2021-05-19 16:14:22 +0300

    UserPreferenceActivity is updated

--- app/src/main/java/com/elf/dea/UserData/EatingPreferences.java
+++ app/src/main/java/com/elf/dea/UserData/EatingPreferences.java
@@ -1,11 +1,28 @@
 package com.elf.dea.UserData;
 
 public class EatingPreferences {
-    Likes likes;
-    Dislikes dislikes;
+        boolean meat;
+        boolean fish;
+        boolean fastfood;
+        boolean traditional;
+        boolean coffee;
+        boolean drink;
 
-    public EatingPreferences() {
-        this.likes = new Likes();
-        this.dislikes = new Dislikes();
+        public EatingPreferences(boolean meat, boolean fish, boolean fastfood, boolean traditional, boolean coffee, boolean drink) {
+            this.meat = meat;
+            this.fish = fish;
+            this.fastfood = fastfood;
+            this.traditional = traditional;
+            this.coffee = coffee;
+            this.drink = drink;
+        }
+
+        public EatingPreferences() {
+            this.meat = false;
+            this.fish = false;
+            this.fastfood = false;
+            this.traditional = false;
+            this.coffee = false;
+            this.drink = false;
+        }
     }
-}
--- app/src/main/java/com/elf/dea/UserPreferenceActivity.java
+++ app/src/main/java/com/elf/dea/UserPreferenceActivity.java
@@ -3,12 +3,65 @@ package com.elf.dea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+import com.elf.dea.UserData.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class UserPreferenceActivity extends AppCompatActivity {
+    CheckBox meatCheckBox, fishCheckBox, fastfoodCheckBox,
+            traditionalCheckBox, coffeeCheckBox, drinkCheckBox;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+    User user = new User();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_preference);
+
+        meatCheckBox = findViewById(R.id.MeatCheckBox);
+        fishCheckBox = findViewById(R.id.FishCheckBox);
+        fastfoodCheckBox = findViewById(R.id.FastFoodCheckBox);
+        traditionalCheckBox = findViewById(R.id.TraditionalCheckBox);
+        coffeeCheckBox = findViewById(R.id.CoffeeCheckBox);
+        drinkCheckBox = findViewById(R.id.DrinkCheckBox);
+
     }
+
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case meatCheckBox:
+                if (checked)
+            else
+                // Remove the meat
+                break;
+            case fishCheckBox:
+                if (checked)
+                // Cheese me
+            else
+                // I'm lactose intolerant
+                break;
+        }
+    }
+
+    public void Next(View view){
+
+    }
+
+    public void Back(View view) {
+
+    }
+
+    public void EditLate(){
+
+    }
+
 }
